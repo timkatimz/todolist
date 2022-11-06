8524512e78e400b8fb2f321f5fbac8da1fe90cef
tim <timrhymer@ya.ru>
2022-11-06 22:57:36 +0400

    set default postgres port

--- todolist/settings.py
+++ todolist/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'PORT': 5432,
     }
 }
 
