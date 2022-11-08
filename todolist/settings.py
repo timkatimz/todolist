bfa4b8b65ca34b54f0b17d51a31cf54c9bfb1084
tim <timrhymer@ya.ru>
2022-11-08 13:54:34 +0400

    set ALLOWED_HOSTS

--- todolist/settings.py
+++ todolist/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.get_value('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
